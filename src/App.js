7ea6362d49a451f9ce8f00e094669a2e53be8f41
Muhammad Rauf <mrauff341@gmail.com>
2022-04-04 00:06:23 +0700

    ramadhan

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, {useState, useDispatch, useEffect} from 'react';
 import './App.css';
 import axios from 'axios'
 
-const App = () => {
+function App() {
+  
+  const [authToken, setAuthToken] = useState("");
+  const [musicData, setMusicData] = useState([]);
+  
   const state={
     searchKey : "",
     gifData: []
@@ -12,41 +16,93 @@ const App = () => {
   const [gifData, setGifData] = useState([]);
   
  const onChange = (e) => {
-   this.setState({
+   this.setSearchKey({
      searchKey: e.target.value
    })
  }
 
-  const handleSearch = async () => {
-    const apikey = '&api_key=jwk3vm89s4NSzG4KlIKLieCMoBL3uO4s';
-    const q = '&q' + this.state.searchKey;
-    const limit = '&limit=12';
-    try {
-      const response = await axios.get(`api.giphy.com/v1/gifs/search?${apikey}${q}${limit}`)
-      if(response && this.setState({gifData})) return;
-    } catch (error) {
-      return error;
+ const handleSearch = async (e) => {
+  e.preventDefault();
+  const url = "https://api.spotify.com/v1/search";
+  const keywords = searchKey;
+  const type = "track";
+  try {
+    const response = await fetch(`${url}?q=${keywords}&type=${type}&limit=10`, {
+      headers: {
+        'Authorization' : 'Bearer ' + authToken
+      }
+    });
+    
+    if (!response.ok) {
+      switch (response.status) {
+        case 401:
+          throw new Error(`Unauthorized access, please login first`);
+        case 403:
+          throw new Error(`Forbidden access`);
+        default:
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } else {
+      const musicData = await response.json()
+      setMusicData(musicData.tracks.items);
     }
-
+  } catch (error) {
+    alert(`There has been a problem with your fetch operation: ${error.message}`);
   }
+};
+
+ 
+
+  // const handleSearch = async () => {
+  //   const apikey = '&api_key=jwk3vm89s4NSzG4KlIKLieCMoBL3uO4s';
+  //   const q = '&q' + this.state.searchKey;
+  //   const limit = '&limit=12';
+  //   try {
+  //     const response = await axios.get(`api.giphy.com/v1/gifs/search?${apikey}${q}${limit}`)
+  //     console.log(response);
+  //     if(response && this.setState({gifData})) return;
+      
+  //   } catch (error) {
+  //     return error;
+  //   }
+
+    
+
+  // }
+
+  // const handleSearch = async () => {
+  //     const url = "api.giphy.com/v1/gifs/search";
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     console.log(data);
+  
   
 
   return (  
     <div className="App">
       <div className="container">
-        <h1>Hello Exercise Module 3 Session 1</h1>
+        <h1>Create Playlist</h1>
         <div className="search-bar-container">
-          <input type="text" onChange={onChange}/>
-          <input type="submit" value="Search" onClick={handleSearch}/>
+          <div className='title'>
+            <h3> Title </h3> 
+            <input type="text" onChange={onChange}/>
+            </div>
+
+          <div className='description'>
+            <h3>description</h3>
+            <input type="text" onChange={onChange}/>
+          </div>
+            <input type="submit" value="Search" onClick={handleSearch}/>
         </div>
         <div>
           {
-            this.state.gifData.map((gif) => <div key={gif.id} title={gif.title} url={gif.images.fixed_width.url}/>)
+            //this.state.gifData.map((gif) => <div key={gif.id} title={gif.title} url={gif.images.fixed_width.url}/>)
           }
         </div>
       </div>
     </div>
-  )
-};
+  );
+
+}
 
 export default App;
